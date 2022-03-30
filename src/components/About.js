1166461e89110e867d02fc65d517e31a7c6e510b
Mostafa Hussein <mahussein04@gmail.com>
2022-03-30 16:44:32 -0400

    react

--- src/components/About.js
+++ src/components/About.js
@@ -44,7 +44,7 @@ function About() {
         <div style={flexCont}>
             <div>
                 <h1 style={titleStyle}>Hey👋</h1>
-                <h3 style={textStyle}>I am Mostafa Hussein! An incoming first year Mechatronics Engineering student at the University of Waterloo with a passion for science and technology. I enjoy programming and electronics, and hope to pursue a career in robotics in the future. Currently, I am looking for new challenges to improve my skills and grow my network. Particularly focusing on web development and machine learning at the moment. Learn more about me by scrolling through my website!</h3>
+                <h3 style={textStyle}>I am Mostafa Hussein! An incoming first year student to Mechatronics Engineering at the University of Waterloo with a passion for science and technology. I enjoy programming and electronics, and hope to pursue a career in robotics in the future. Currently, I am looking for new challenges to improve my skills and grow my network. Particularly focusing on web development and machine learning at the moment. Learn more about me by scrolling through my website!</h3>
                 <div style = {btnSec}>
                 <br />
                     <a href = "https://drive.google.com/file/d/1a3I1CHtxwZnfcyMbPvrXwMktkEdkgcpv/view?usp=sharing" target = "_blank" class = "btns">Resume</a><br /><br /><br />
